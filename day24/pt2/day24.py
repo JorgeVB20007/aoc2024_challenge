69c17c5598d3125e9c846129fc1162e5783e918e
Jorge VB <84627746+JorgeVB20007@users.noreply.github.com>
2025-01-05 19:19:48 +0100

    day24 pt2

--- day24/pt2/day24.py
+++ day24/pt2/day24.py
@@ -6,57 +6,6 @@ values_and_ops = text.split("\n\n")
 initial_values_lines = values_and_ops[0].split("\n")
 initial_ops_lines = values_and_ops[1].split("\n")
 
-
-
-
-values_dict = {}
-for value in initial_values_lines:
-	splitvalue = value.split(": ")
-	values_dict[splitvalue[0]] = int(splitvalue[1])
-
-
-expected_result = 0
-
-xs = []
-ys = []
-for xy in values_dict:
-	if xy[0] == 'x':
-		xs.append((xy, values_dict[xy]))
-	elif xy[0] == 'y':
-		ys.append((xy, values_dict[xy]))
-xs.sort()
-xs.reverse()
-ys.sort()
-ys.reverse()
-
-xval = 0
-for x in xs:
-	xval *= 2
-	xval += x[1]
-yval = 0
-for y in ys:
-	yval *= 2
-	yval += y[1]
-
-print(xval)
-print(yval)
-zval = xval + yval
-print(zval)
-
-zval_bin = []
-while zval > 0:
-	zval_bin.insert(0, zval % 2)
-	zval = int((zval - zval % 2) / 2)
-
-expected_zs = {}
-for idx in range(0, len(zval_bin)):
-	expected_zs["z{:0>2}".format(idx)] = zval_bin[len(zval_bin) - idx - 1]
-
-
-
-print(expected_zs)
-
-
 ops_list = []
 
 for op in initial_ops_lines:
@@ -67,50 +16,48 @@ for op in initial_ops_lines:
 	op_dict["second"] = splitop[2]
 	op_dict["result"] = splitop[4]
 	ops_list.append(op_dict)
-# print(ops_list)
 
-everything_solved = False
-while not everything_solved:
-	everything_solved = True
+
+def get_full_operations_by_operands(operand: str) -> list:
+	result = []
+	for curr in ops_list:
+		if curr["first"] == operand or curr["second"] == operand:
+			result.append(curr)
+	return result
+
+
+def find_sus_operations():
+	result = []
 	for op in ops_list:
-		if op["first"] in values_dict and op["second"] in values_dict:
-			this_result = 0
-			if op["operation"] == 'AND':
-				result = int(bool(values_dict[op["first"]]) and bool(values_dict[op["second"]]))
-			elif op["operation"] == 'XOR':
-				result = int(values_dict[op["first"]] != values_dict[op["second"]])
-			else:
-				result = int(bool(values_dict[op["first"]]) or bool(values_dict[op["second"]]))
-			values_dict[op["result"]] = result
-		else:
-			everything_solved = False
-
-# print(values_dict)
-
-
-final_zs = []
-for idx in values_dict:
-	if idx[0] == 'z':
-		final_zs.append((idx, values_dict[idx]))
-
-final_zs.sort()
-
-for z in range(len(final_zs)):
-	print(str(z) + ':', final_zs[z], expected_zs["z{:0>2}".format(z)])
-final_zs.reverse()
-
-result = 0
-for idx in final_zs:
-	result *= 2
-	result += idx[1]
+		if op["result"][0] == 'z':
+			if op["operation"] != 'XOR' and op["result"][1:] != '45':
+				result.append(op["result"])
+		elif not (op["first"][0] == 'x' or op["first"][0] == 'y'):
+			if op["operation"] == "XOR":
+				result.append(op["result"])
+		elif op["operation"] == "XOR" and op["first"] != 'x00' and op["first"] != 'y00':
+			next_options = get_full_operations_by_operands(op["result"])
+			possible = False
+			for optn in next_options:
+				if optn["operation"] == "XOR":
+					possible = True
+			if not possible:
+				result.append(op["result"])
+		elif op["operation"] == "AND" and op["first"] != 'x00' and op["first"] != 'y00':
+			next_options = get_full_operations_by_operands(op["result"])
+			possible = False
+			for optn in next_options:
+				if optn["operation"] == "OR":
+					possible = True
+			if not possible:
+				result.append(op["result"])
 
 
-print(">>>", result)
+	result.sort()
+	resultstr = ""
+	for x in result:
+		resultstr = resultstr + x + ','
+	resultstr = resultstr[:-1]
+	print(">>>", resultstr)
 
-
-
-
-
-
-
-
+find_sus_operations()
